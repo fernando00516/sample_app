40df35423605dbe9d7e84b4dec68b6df84faa699
shiomifernando <tls0516@icloud.com>
2019-04-03 21:00:05 +0900

    Add password and password confirmation value to user_test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com"),
+     password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -45,7 +46,7 @@ class UserTest < ActiveSupport::TestCase
    @user.save
    assert_not duplicate_user.valid?
  end
- 
+
  test "email addresses should be saved as lower-case" do
    mixed_case_email = "Foo@ExAMPle.CoM"
    @user.email = mixed_case_email
