ea26cd9acff813e74a65994a490b1380a807700d
shiomifernando <tls0516@icloud.com>
2019-04-03 11:58:32 +0900

    modify users_routes,new_html,controller_test

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   root 'static#home'
   get  '/help',    to: 'static#help'
- get  '/about',   to: 'static#about'
- get  '/contact', to: 'static#contact'
+  get  '/about',   to: 'static#about'
+  get  '/contact', to: 'static#contact'
+  get '/signup', to: 'users#new'
 end
