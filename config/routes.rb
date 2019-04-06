d8a8941d49227b83969a3b027a4b01ec28c14ab6
shiomifernando <tls0516@icloud.com>
2019-04-07 00:15:04 +0900

    add roote for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  
   root 'static#home'
   get  '/help',    to: 'static#help'
   get  '/about',   to: 'static#about'
   get  '/contact', to: 'static#contact'
   get '/signup', to: 'users#new'
   post '/signup',  to: 'users#create'
+  get '/login', to: 'session#new'
+  post '/login', to: 'session#create'
+  delete '/login', to: 'session#destroy'
   resources :users
 end
