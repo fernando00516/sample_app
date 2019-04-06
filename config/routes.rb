65d7f2140d66d8a1d2533b204e4d3abe89abd7f6
shiomifernando <tls0516@icloud.com>
2019-04-06 15:40:17 +0900

    Add routes post_signup_creat

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static#about'
   get  '/contact', to: 'static#contact'
   get '/signup', to: 'users#new'
+  post '/signup',  to: 'users#create'
   resources :users
 end
