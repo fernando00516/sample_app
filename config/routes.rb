c9e0ba34fd5547bfb464d3c3fb081d05a93de155
shiomifernando <tls0516@icloud.com>
2019-04-07 00:26:51 +0900

    Add login_form view

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  
+
   root 'static#home'
   get  '/help',    to: 'static#help'
   get  '/about',   to: 'static#about'
   get  '/contact', to: 'static#contact'
   get '/signup', to: 'users#new'
   post '/signup',  to: 'users#create'
-  get '/login', to: 'session#new'
-  post '/login', to: 'session#create'
-  delete '/login', to: 'session#destroy'
+  get '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
 end
