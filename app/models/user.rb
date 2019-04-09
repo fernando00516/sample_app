7cae5c11b1f5d6f0353684a2290d6f0dda9da719
shiomifernando <tls0516@icloud.com>
2019-04-09 19:39:34 +0900

    Modify model_user.rb Add a method for token generation

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,12 @@ class User < ApplicationRecord
    validates :password, presence: true, length: { minimum: 6 }
 
    def User.digest(string)
-   cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                  BCrypt::Engine.cost
-   BCrypt::Password.create(string, cost: cost)
- end
+                                                 BCrypt::Password.create(string, cost: cost)
+   end
+
+   def User.new_token
+    SecureRandom.urlsafe_base64
+  end
 end
