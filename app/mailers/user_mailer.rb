f45f0fb72d7fd3c5fbd1f7b1f8a27d8a6dd3f0c9
shiomifernando <tls0516@icloud.com>
2019-04-21 02:18:24 +0900

    modify user_mailer Email account activation link

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,21 +1,10 @@
 class UserMailer < ApplicationMailer
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.acount_activation.subject
-  #
-  def acount_activation
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def acount_activation(user)
+    @user = user
+    mail to: user.mail,subject: "Account activation"
   end
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.password_reset.subject
-  #
   def password_reset
     @greeting = "Hi"
 
