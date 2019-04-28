2abec18518c1bde2b7719705a8e78c8e655339ae
shiomifernando <tls0516@icloud.com>
2019-04-28 12:48:45 +0900

    Error correction

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
     :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
-  }config.action_mailer.perform_caching = false
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
